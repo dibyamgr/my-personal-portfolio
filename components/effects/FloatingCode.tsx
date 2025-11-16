ac934be34d5a4aa378219306b0cb50efddde1d88
dibyamgr <dibyamagar5@gmail.com>
2025-11-15 21:16:59 -0330

    Floating Code Effect

---
+++ components/effects/FloatingCode.tsx
@@ -0,0 +1,22 @@
+const FloatingCode = () => {
+  return (
+    <div className="floating-code">
+      <div className="code-line">const developer = {"{"}</div>
+      <div className="code-line"> name: &apos;Dibya RS Magar&apos;,</div>
+      <div className="code-line"> role: &apos;Full Stack Developer&apos;,</div>
+      <div className="code-line">
+        {" "}
+        skills: [&apos;React&apos;, &apos;Node&apos;, &apos;Python&apos;],
+      </div>
+      <div className="code-line">
+        {" "}
+        passion: &apos;Creating Awesome Stuff&apos;,
+      </div>
+      <div className="code-line"> coffeeLevel: Infinity,</div>
+      <div className="code-line"> available: true,</div>
+      <div className="code-line">{"}"}</div>
+    </div>
+  );
+};
+
+export default FloatingCode;
